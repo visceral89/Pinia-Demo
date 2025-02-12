7bb5c1878de47e890799ab677c5d457650fd6a5d
Rasmus Bremholm <rasmus.brem@gmail.com>
2025-02-12 14:12:46 +0100

    Its working!

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -12,8 +12,8 @@ export default defineComponent({
 	<div class="task">
 		<h3>{{ task.title }}</h3>
 		<div class="icons">
-			<i class="material-icons delete">delete</i>
-			<i class="material-icons favorite">favorite</i>
+			<i class="material-icons delete" @click="taskStore.deleteTask(task.id)">delete</i>
+			<i class="material-icons favorite" @click="taskStore.importantTask(task.id)">favorite</i>
 		</div>
 	</div>
 </template>
